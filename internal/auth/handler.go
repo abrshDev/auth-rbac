92b36a3d2293f682099311e537fabb8daffbe9bf
abrshDev <abrshmelkamu3@gmail.com>
2026-02-27 16:23:42 +0300

    login added

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -2,6 +2,8 @@ package auth
 
 import (
 	"github.com/abrshDev/auth-rbac/internal/user"
+
+	"github.com/abrshDev/auth-rbac/pkg/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -14,6 +16,10 @@ type RegisterRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 	Role     string `json:"role"` // optional
+}
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func NewAuthHandler(repo *user.Repository) *AuthHandler {
@@ -38,5 +44,26 @@ func (h *AuthHandler) Register(c *fiber.Ctx) error {
 		"username": user.Username,
 		"email":    user.Email,
 		"role":     user.Role,
+	})
+}
+
+func (h *AuthHandler) Login(c *fiber.Ctx) error {
+	var body LoginRequest
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	user, err := h.repo.GetUserByEmail(body.Email)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid credentials"})
+	}
+
+	if !utils.CheckPassword(user.PasswordHash, body.Password) {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid credentials"})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "login successful",
 	})
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -32,6 +32,7 @@ func ConnectDb() {
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
 	)
+	//	log.Println("CONNECTION:", dsn)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
