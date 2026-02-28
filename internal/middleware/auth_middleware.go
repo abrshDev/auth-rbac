a1058e9dbec73cd2fff28faa9a4710a28917f354
abrshDev <abrshmelkamu3@gmail.com>
2026-02-28 18:29:24 +0300

    auth_middleware

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -1,1 +1,42 @@
 package middleware
+
+import (
+	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func Protected() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+
+		authHeader := c.Get("Authorization")
+		if authHeader == "" {
+			return c.Status(fiber.StatusUnauthorized).
+				JSON(fiber.Map{"error": "Missing authorization header"})
+		}
+
+		tokenString := strings.Split(authHeader, " ")
+		if len(tokenString) != 2 {
+			return c.Status(fiber.StatusUnauthorized).
+				JSON(fiber.Map{"error": "Invalid token format"})
+		}
+
+		token, err := jwt.Parse(tokenString[1], func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+
+		if err != nil || !token.Valid {
+			return c.Status(fiber.StatusUnauthorized).
+				JSON(fiber.Map{"error": "Invalid or expired token"})
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+
+		c.Locals("user_id", claims["user_id"])
+		c.Locals("role", claims["role"])
+
+		return c.Next()
+	}
+}
